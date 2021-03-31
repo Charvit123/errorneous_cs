7aae13aac759b0804372df7ccc0240d19afffe8e
Charvit123 <67510331+Charvit123@users.noreply.github.com>
2021-03-31 11:52:46 +0530

    Css to question part

--- kmarket/urls.py
+++ kmarket/urls.py
@@ -43,14 +43,10 @@ urlpatterns = [
     #     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
     #          name='password_reset_complete'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset/password_reset.html",
-                                                                 success_url=reverse_lazy('password_reset_done')), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
-        template_name="password_reset/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_form.html",
-                                                                                success_url=reverse_lazy('password_reset_complete')), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name="password_reset/password_reset_done.html"), name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset/password_reset.html",success_url=reverse_lazy('password_reset_done')), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_form.html",success_url=reverse_lazy('password_reset_complete')), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset/password_reset_done.html"), name="password_reset_complete"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
